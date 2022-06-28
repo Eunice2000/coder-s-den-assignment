16118ddf7be51bbc0884ce4f041dbab29202531b
Eunice2000 <adediraneunice20@gmail.com>
2022-06-29 00:38:08 +0100

    initcoders den assignment

--- introduction.js
+++ introduction.js
@@ -4,7 +4,11 @@
 "2 Write another single comment which says, Welcome to 30DaysOfJavaScript"
 //Welcome to 30DaysOfJavaScript
 "3 Write a multiline comment which says, comments can make code readable easy to reuse and informative"
-/*comments can make code readable easy to reuse and informative*/
+/*comments can make code readable easy to reuse and informative
+comments can make code readable easy to reuse and informative
+comments can make code readable easy to reuse and informative
+comments can make code readable easy to reuse and informative
+*/
 "6 Declare four variables without assigning values"
 let schoolName
 let specialization
@@ -12,25 +16,25 @@ let localGovernment
 let city
 
 //"7 Declare four variables with assigned values"
-let surName="Adediran";
-let town="Ilorin";
-let state="kwara";
-let department="computer science";
+let surName="Adediran"
+let town="Ilorin"
+let state="kwara"
+let department="computer science"
 
 //"8 Declare variables to store your first name, last name, marital status, country and age in multiple lines"
-let firstName="Eunice";
-let last_Name="Adebukola";
-let maritalStatus="single";
-let country="Nigeria";
-let age="18";
+let firstName="Eunice"
+let last_Name="Adebukola"
+let maritalStatus="single"
+let country="Nigeria"
+let age="18"
 console.log(firstName)
 console.log (last_Name)
 console.log(maritalStatus)
 console.log( country)
 console.log(age)
 //"9 Declare variables to store your first name, last name, marital status, country and age in a single line"
-console.log(firstName,lastName,maritalStatus, country,age)
+console.log(firstName,last_Name,maritalStatus, country,age)
 //"10  Declare two variables myAge and yourAge and assign them initial values and log to the browser console."
-let myAge="I am 25 years old.";
-let yourAge="You are 30 years old.";
+let myAge="I am 25 years old."
+let yourAge="You are 30 years old."
 console.log(myAge,yourAge)
